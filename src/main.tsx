8792d8fbfbc5aaf02bc2f217b2cf77995dc1baff
ro <rosendoinzunza@gmail.com>
2025-08-02 11:20:07 -0700

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-<BrowserRouter basename="/my-portfolio">
+<BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutPage />} />
