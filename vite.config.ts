75bb865bcd4c45dfd71a50f00ffa16cee11db483
ro <rosendoinzunza@gmail.com>
2025-08-02 17:45:35 -0700

    Fix GitHub Pages routing with 404.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
-base: './', 
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, 'docs/index.html'),
+          dest: '.', // Will be copied to docs/404.html
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: 'docs',
   },
